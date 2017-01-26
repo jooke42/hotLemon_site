b3a2b33b51c5ae672e8803027401e3351298799b
miskinalien <alizee.perrin@hotmail.fr>
2017-01-26 10:38:24 +0100

    Petits bugs réparés
    -bug du scroll réparé
    -ajout d'une image utilisateur (lol)

--- App.jsx
+++ App.jsx
@@ -5,15 +5,10 @@ var SideBar = require('react-burger-menu').slide;
 class App extends React.Component {
    render() {
       return (
-         <div>
+         <div id = "container">
             <NavBar/>
-			<SideBar>
-				<a id="home" className="menu-item" href="/">Home</a>
-				<a id="about" className="menu-item" href="/about">About</a>
-				<a id="contact" className="menu-item" href="/contact">Contact</a>
-				<a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-      		</SideBar>
-
+			<SideNav/>
+			<Bloc/>
          </div>
       );
    }
@@ -22,25 +17,47 @@ class App extends React.Component {
 class NavBar extends React.Component {
    render() {
       return (
-			<div className = "navbar">
-				<div id = "logo">
-					<a href = "http://eelslap.com/"><img src="img\HotLemon_Logo.png"/></a>
-				</div>
-				<div id = "buttons">
-					<ul>
-						<li><a href="#">Trending</a></li>
-						<li><a href="#">Chocolate</a></li>
-						<li><a href="#">Cuteness</a></li>
-						<li><a href="#">Lapins</a></li>
-						<li><a href="#">Chatons</a></li>
-						<li><a href="#">A propos</a></li>
-					</ul>
-				</div>			
+		<div className = "navbar">
+			<div id = "logo">
+				<a href = "http://eelslap.com/"><img src="img\HotLemon_Logo.png"/></a>
 			</div>
-		
+			<div id = "buttons">
+				<ul>
+					<li><a href="#">Trending</a></li>
+					<li><a href="#">Chocolate</a></li>
+					<li><a href="#">Cuteness</a></li>
+					<li><a href="#">Lapins</a></li>
+					<li><a href="#">Chatons</a></li>
+					<li><a href="#">A propos</a></li>
+				</ul>
+			</div>			
+		</div>
       );
    }
 }
 
+class SideNav extends React.Component {
+   render() {
+      return (
+		<SideBar width={250} noOverlay>	
+			<img src = "\img\userpic.png"></img>
+			<a id="home" className="menu-item" href="#">Home</a>
+			<a id="about" className="menu-item" href="#">About</a>
+			<a id="contact" className="menu-item" href="#">Contact</a>
+			<a id="settings" className="menu-item" href="#">Settings</a>			
+		</SideBar>
+      );
+   }
+}
+
+class Bloc extends React.Component {
+   render() {
+      return (
+		<div>
+			
+		</div>
+      );
+   }
+}
 
 export default App;
