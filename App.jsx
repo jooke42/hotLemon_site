fd8ca431a884f8a253c63a9c8d5b2ef3a93de9b1
miskinalien <alizee.perrin@hotmail.fr>
2017-05-04 11:44:07 +0200

    Commit 04/05

--- App.jsx
+++ App.jsx
@@ -30,6 +30,7 @@ class NavBar extends React.Component {
 					<li><a href="#">SONDAGES</a></li>
 					<li><a href="#">GALERIE PHOTO</a></li>
 					<li><a href="#">COMPTE</a></li>
+					<li><a href="#">ECRIRE QUELQUE CHOSE</a></li>
 				</ul>
 			</div>			
 		</div>
@@ -37,33 +38,15 @@ class NavBar extends React.Component {
    }
 }
 
-
-/*
-class SideNav extends React.Component {
-   render() {
-      return (
-		<SideBar width={200} noOverlay>	
-			<img src = "\img\userpic.png"></img>
-			<a id="home" className="menu-item" href="#">Accueil</a>
-			<a id="profile" className="menu-item" href="#">Votre profil</a>
-			<a id="settings" className="menu-item" href="#">Vos options</a>
-			<a id="faq" className="menu-item" href="#">FAQ</a>
-			<a id="contact" className="menu-item" href="#">Nous contacter</a>			
-		</SideBar>
-      );
-   }
-}
-*/
-
 class Feed extends React.Component{
 	render() {
 		return (
 			<div className = "feed">
 				<Article/>
 				<Article/>
+				<Sondage/>
 				<Evenement/>
-				<Evenement/>
-				<Article/>
+				<Sondage/>
 				<Article/>	
 				<Evenement/>
 				<Article/>
@@ -77,9 +60,10 @@ class Article extends React.Component {
    render() {
       return (
 		<div className = "article">
-			<h2>Title sample</h2>
-			<p>C’est dans un écosystème appartenant au Mont Kaputar situé dans la région du New South Wales en Australie que vient d’être tout récemment découvert une espèce tout à fait surprenante de limace. Un invertébré de couleur rose et de surcroit… fluorescent! Michael Murphy, un ranger du National Parks and Wildlife Service exerçant ses fonctions dans la région de la Nouvelle-Galles du Sud en Australie, vient d’être l’un des premiers témoins de la vie d’une forme tout à fait particulière de limace encore jamais observée dans le passé. C’est sur les hauteurs du Mont Kaputar, là où la forêt alpine semble abriter des espèces encore inconnues de tous, que l’homme vient en effet de faire la découverte d’une limace géante entièrement rose et fluorescente.</p>
+			<h2>Titre de l'article</h2>
+			<p>Description de l'article. L'utilisateur devra cliquer pour voir la suite.</p>
 		</div>
+		
       );
    }
 }
@@ -88,11 +72,24 @@ class Evenement extends React.Component {
 	render() {
 		return (
 			<div className = "evenement">
-				<h2>Moules frites à volonté</h2>
-				<h4>Vaucresson City Center</h4>
+				<h2>Nom de l'évènement</h2>
+				<h4>Lieu de l'évènement</h4>
+				<h5>Nom de l'organisateur</h5>
 				<h5>15/05/2017 à 20:30</h5>
-				
-				<p> Benji le Ravioli Dupont-Aignant vous invite à déguster des moules frites à un délicieux buffet concocté par la maman de Stephen. Des activité annexes sont prévues, telles que strip-tease et cracheurs de flammes.</p>
+			</div>
+		);
+	}
+}
+
+class Sondage extends React.Component {
+	render() {
+		return (
+			<div className = "sondage">
+				<h2> Question du sondage </h2>
+				<p id = "button_sondage"> Réponse1 </p>
+				<p id = "button_sondage"> Réponse2 </p>
+				<h6> XXXX personnes ont répondu !</h6>
+				<h4> <a href="#"> Voir le sondage </a></h4>
 			</div>
 		);
 	}
